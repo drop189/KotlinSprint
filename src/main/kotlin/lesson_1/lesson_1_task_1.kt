b39025a3ea9d4f61c55798223875b78d8731d82f
drop189 <71636973+drop189@users.noreply.github.com>
2024-04-21 19:33:10 +0300

    KS-1-2 delete task1 from branch of task2

--- src/main/kotlin/lesson_1/lesson_1_task_1.kt
+++
@@ -1,7 +0,0 @@
-package org.example.lesson_1
-
-fun main() {
-    val numberOfOrders: Int = 75
-    val textCustomerMakePurchase: String =
-        "Спасибо, что выбрали нас! Мы стремимся предлагать вам самые лучшие товары. Если у вас есть какие-либо вопросы, не стесняйтесь обращаться к нам."
-}

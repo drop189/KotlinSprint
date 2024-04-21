2170ec6cec33064cbd92bf05dc94c639b5d3ab9f
drop189 <71636973+drop189@users.noreply.github.com>
2024-04-21 20:02:20 +0300

    KS-1-4 do task4

---
+++ src/main/kotlin/lesson_1_task_4.kt
@@ -0,0 +1,18 @@
+package org.example
+
+fun main() {
+
+    val length: Long = 40_868_600_000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+}

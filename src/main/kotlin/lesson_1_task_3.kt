2c635f266f6414e5a438f6a4504b48ae6cff4c7c
drop189 <71636973+drop189@users.noreply.github.com>
2024-04-21 19:45:10 +0300

    KS-1-3 do task3

---
+++ src/main/kotlin/lesson_1_task_3.kt
@@ -0,0 +1,20 @@
+package org.example
+
+fun main(){
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    println()
+
+    hour = 10
+    minute = 55
+
+    print(hour)
+    print(":")       //println("$hour:$minute")
+    print(minute)
+}

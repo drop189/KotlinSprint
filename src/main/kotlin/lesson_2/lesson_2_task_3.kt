c12cd337e64b2ca12215fd03a84a275dc8f142d5
drop189 <71636973+drop189@users.noreply.github.com>
2024-04-22 21:22:34 +0300

    KS-2-3 refresh task3

--- src/main/kotlin/lesson_2/lesson_2_task_3.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_3.kt
@@ -5,13 +5,18 @@ const val SIXTY = 60
 fun main() {
     val departureHour = 9
     val departureMinute = 39
-    val timeInMinutes = 437
+    val timeInMinutes = 457
 
     val travelTimeInHours = timeInMinutes / SIXTY
     val travelTimeInMinutes = timeInMinutes % SIXTY
 
-    val arrivalTimeInHours = departureHour + travelTimeInHours
-    val arrivalTimeInMinutes = departureMinute + travelTimeInMinutes
+    var arrivalTimeInHours = departureHour + travelTimeInHours
+    var arrivalTimeInMinutes = departureMinute + travelTimeInMinutes
+
+    if (arrivalTimeInMinutes > 60) {
+        arrivalTimeInMinutes -= 60
+        arrivalTimeInHours += 1
+    }
 
     println("$arrivalTimeInHours:$arrivalTimeInMinutes")
 }

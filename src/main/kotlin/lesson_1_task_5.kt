d5aaf1dfee1ba3596c591f4bed82a0ae049c0d63
drop189 <71636973+drop189@users.noreply.github.com>
2024-04-22 10:11:26 +0300

    KS-1-5 add constant

--- src/main/kotlin/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1_task_5.kt
@@ -1,11 +1,12 @@
 package org.example
 
+const val sixty = 60
 fun main() {
     val seconds = 6480
-    val minutes = seconds / 60
-    val hours = minutes / 60
-    val remainsSeconds = seconds % 60
-    val remainsMinutes = minutes % 60
+    val minutes = seconds / sixty
+    val hours = minutes / sixty
+    val remainsSeconds = seconds % sixty
+    val remainsMinutes = minutes % sixty
 
     println(
         hours.toString().padStart(2, '0') +

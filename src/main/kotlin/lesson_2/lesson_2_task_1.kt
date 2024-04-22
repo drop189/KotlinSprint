3899d94e70eec747511ebe105eee8657aaa7a401
drop189 <71636973+drop189@users.noreply.github.com>
2024-04-22 21:11:37 +0300

    KS-2-1 refresh task5

--- src/main/kotlin/lesson_2/lesson_2_task_1.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_1.kt
@@ -1,13 +1,15 @@
 package org.example.lesson_2
 
-const val NUMBER_OF_STUDENTS = 4.0
-const val GRADE_OF_PETYA = 3.0
-const val GRADE_OF_IVAN = 4.0
-const val GRADE_OF_ANYA = 3.0
-const val GRADE_OF_DIANA = 5.0
 fun main() {
 
-    val result = ((GRADE_OF_PETYA + GRADE_OF_IVAN + GRADE_OF_ANYA + GRADE_OF_DIANA) / NUMBER_OF_STUDENTS)
+    val numberOfStudents = 4
+    val gradeOfPetya = 3.0
+    val gradeOfIvan = 4.0
+    val gradeOfAnya = 3.0
+    val gradeOfDiana = 5.0
 
-    println(result)
+
+    val averageGrade = ((gradeOfPetya + gradeOfIvan + gradeOfAnya + gradeOfDiana) / numberOfStudents)
+
+    println(averageGrade)
 }

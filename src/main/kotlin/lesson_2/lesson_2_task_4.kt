851af44d536d957ee9078316382c8d888698256c
drop189 <71636973+drop189@users.noreply.github.com>
2024-04-22 21:34:18 +0300

    KS-2-4 refresh task4

--- src/main/kotlin/lesson_2/lesson_2_task_4.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_4.kt
@@ -1,12 +1,14 @@
 package org.example.lesson_2
 
-const val PLUS_TWENTY_PERCENT = 1.20
 fun main() {
+    val percent = 20
+    val plusPercent= ((1 + (percent.toDouble()/100)))
+
     val extractedCrystals = 7
     val extractedIronOre = 11
 
-    val extractedCrystalsWithBuff= extractedCrystals * PLUS_TWENTY_PERCENT
-    val extractedIronOreWithBuff = extractedIronOre * PLUS_TWENTY_PERCENT
+    val extractedCrystalsWithBuff= extractedCrystals * plusPercent
+    val extractedIronOreWithBuff = extractedIronOre * plusPercent
 
 
     println(extractedCrystalsWithBuff.toInt())

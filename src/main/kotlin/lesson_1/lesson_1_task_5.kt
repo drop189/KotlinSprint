8265e590b3bf19c5b6ef268de075d7caf3c1502b
drop189 <71636973+drop189@users.noreply.github.com>
2024-04-23 19:56:40 +0300

    KS-1-5 refresh task5

--- src/main/kotlin/lesson_1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_5.kt
@@ -1,13 +1,13 @@
 package org.example.lesson_1
 
-const val sixty = 60
-const val seconds = 6480
-fun main() {
+const val SECONDS_IN_HOUR = 60
 
-    val minutes = seconds / sixty
-    val hours = minutes / sixty
-    val remainsSeconds = seconds % sixty
-    val remainsMinutes = minutes % sixty
+fun main() {
+    val seconds = 6480
+    val minutes = seconds / SECONDS_IN_HOUR
+    val hours = minutes / SECONDS_IN_HOUR
+    val remainsSeconds = seconds % SECONDS_IN_HOUR
+    val remainsMinutes = minutes % SECONDS_IN_HOUR
 
     println("%02d:%02d:%02d".format(hours, remainsMinutes, remainsSeconds))
 }

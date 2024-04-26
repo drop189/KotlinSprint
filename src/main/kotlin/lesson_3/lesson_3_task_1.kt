5ea0147462ed7ca90fd91eb315929ee26f42652b
drop189 <71636973+drop189@users.noreply.github.com>
2024-04-26 21:06:24 +0300

    KS-3-1 refresh task1

--- src/main/kotlin/lesson_3/lesson_3_task_1.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_1.kt
@@ -2,11 +2,11 @@ package org.example.lesson_3
 
 fun main() {
     val userName = "Лия"
-    var helloText = "Доброе день,"
+    var helloText = "Доброе день"
 
-    println("$helloText $userName")
+    println("$helloText, $userName")
 
-    helloText = "Добрый вечер,"
+    helloText = "Добрый вечер"
 
-    println("$helloText $userName")
+    println("$helloText, $userName")
 }

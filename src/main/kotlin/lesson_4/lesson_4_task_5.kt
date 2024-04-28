43c568e21de947204f41ea96abb87a59e536ba8a
drop189 <71636973+drop189@users.noreply.github.com>
2024-04-28 17:32:38 +0300

    KS-4-5 do task5

--- src/main/kotlin/lesson_4/lesson_4_task_5.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_5.kt
@@ -5,23 +5,32 @@ package org.example.lesson_4
 const val MIN_COMPOSITION_OF_THE_CREW = 55
 const val MAX_COMPOSITION_OF_THE_CREW = 70
 const val MIN_NUMBER_OF_BOXES_OF_PROVISIONS = 50
+const val FAVORABLE_WEATHER_CONDITIONS = true
+const val HAS_DAMAGE = true
 
 fun main() {
 
-    val damageToHull = readln().toBoolean()
+    println("Имеет ли корабль повреждения?")
+    val hasDamage = readln().toBoolean()
+
+    println("Сколько на борту экипажа?")
     val compositionOfTheCrew = readln().toInt()
+
+    println("Сколько на борту ящиков провизии?")
     val numberOfBoxesOfProvisions = readln().toInt()
-    val favorableWeatherConditions = readln().toBoolean()
+
+    println("Благоприятные ли условия погоды?")
+    val isFavorableWeatherConditions = readln().toBoolean()
 
     val longTermSwimmingComparison =
-        (!damageToHull
+        (hasDamage != HAS_DAMAGE
                 && compositionOfTheCrew in (MIN_COMPOSITION_OF_THE_CREW..MAX_COMPOSITION_OF_THE_CREW)
                 && numberOfBoxesOfProvisions > MIN_NUMBER_OF_BOXES_OF_PROVISIONS)
                 ||
-                (damageToHull
+                (hasDamage == HAS_DAMAGE
                         && compositionOfTheCrew == MAX_COMPOSITION_OF_THE_CREW
                         && numberOfBoxesOfProvisions >= MIN_NUMBER_OF_BOXES_OF_PROVISIONS
-                        && favorableWeatherConditions)
+                        && isFavorableWeatherConditions == FAVORABLE_WEATHER_CONDITIONS)
 
-    println(longTermSwimmingComparison)
+    println("Дано ли разрешение спустить корабль на воду - $longTermSwimmingComparison")
 }

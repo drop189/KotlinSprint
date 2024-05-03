6ddaf9afd94f408bb03c620916fde5aa88fb7487
drop189 <71636973+drop189@users.noreply.github.com>
2024-05-03 18:59:44 +0300

    KS-7-4 refresh task4

--- src/main/kotlin/lesson_7/lesson_7_task_4.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_4.kt
@@ -2,12 +2,12 @@ package org.example.lesson_7
 
 fun main(){
     println("Введите количество секунд для таймера:")
-    val userInputSeconds = readln().toInt()
+    var userInputSeconds = readln().toInt()
 
     println("Таймер запущен на $userInputSeconds секунд.")
 
     for (remainSeconds in 1..userInputSeconds){
-        println("Осталось секунд: $remainSeconds")
+        println("Осталось секунд: ${userInputSeconds--}")
         Thread.sleep(1000)
     }
 

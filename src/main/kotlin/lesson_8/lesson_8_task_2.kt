48d78f7e6af8e9089c99427f7b1ebde875fd5337
drop189 <71636973+drop189@users.noreply.github.com>
2024-05-05 10:08:59 +0300

    KS-8-2 do task2

--- src/main/kotlin/lesson_8/lesson_8_task_2.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_2.kt
@@ -6,7 +6,13 @@ fun main() {
     println("Введите название ингредиента который хотите найти:")
 
     val userInput = readln()
+    var isContains = false
 
-    if (arrayOfIngredients.contains(userInput)) println("Ингредиент $userInput в рецепте есть")
+    for (elements in arrayOfIngredients){
+        if (elements == userInput)  isContains = true
+    }
+
+    if (isContains) println("Ингредиент $userInput в рецепте есть")
     else println("Такого ингредиента в рецепте нет")
+
 }

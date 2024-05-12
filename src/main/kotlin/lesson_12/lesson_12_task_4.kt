35bb350a5073aace93e31dc0d8882de891d09533
drop189 <71636973+drop189@users.noreply.github.com>
2024-05-12 19:33:04 +0300

    KS-12-4 refresh task4

--- src/main/kotlin/lesson_12/lesson_12_task_4.kt
+++ src/main/kotlin/lesson_12/lesson_12_task_4.kt
@@ -1,15 +1,15 @@
 package org.example.lesson_12
 
-const val SCALE_FROM_KELVIN_TO_CELSIUS_IN_INT = 273
+const val KELVIN_TO_CELSIUS_CONVERSION_OFFSET = 273
 
 class TemperatureFromKelvinToCelsiusAndPrecipitation(
     daytimeTemperatureInKelvin: Int,
     nighttimeTemperatureInKelvin: Int,
     presenceOfPrecipitationInKelvin: String,
 ) {
-    private var daytimeTemperatureInCelsius: Int = daytimeTemperatureInKelvin - SCALE_FROM_KELVIN_TO_CELSIUS_IN_INT
+    private var daytimeTemperatureInCelsius: Int = daytimeTemperatureInKelvin - KELVIN_TO_CELSIUS_CONVERSION_OFFSET
 
-    private var nighttimeTemperatureInCelsius: Int = nighttimeTemperatureInKelvin - SCALE_FROM_KELVIN_TO_CELSIUS_IN_INT
+    private var nighttimeTemperatureInCelsius: Int = nighttimeTemperatureInKelvin - KELVIN_TO_CELSIUS_CONVERSION_OFFSET
 
     private var presenceOfPrecipitationInCelsius: String = presenceOfPrecipitationInKelvin
 

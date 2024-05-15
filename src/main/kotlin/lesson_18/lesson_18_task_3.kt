eba87a520fe73bc2a4760c1a277d1e8ff330ed7a
drop189 <71636973+drop189@users.noreply.github.com>
2024-05-15 23:53:23 +0300

    KS-18-3 do task3

---
+++ src/main/kotlin/lesson_18/lesson_18_task_3.kt
@@ -0,0 +1,38 @@
+package org.example.lesson_18
+
+open class Tamagotchi(open val name: String){
+    open fun printFoodInfo(){
+        print("[$name] -> ") // [действие]
+    }
+}
+
+class Fox(override val name: String, private val food: String = "Ягоды" ): Tamagotchi(""){
+    override fun printFoodInfo() {
+        super.printFoodInfo()
+        println("[$food]")
+    }
+}
+class Dog(override val name: String, private val food: String = "Кости" ): Tamagotchi(""){
+    override fun printFoodInfo() {
+        super.printFoodInfo()
+        println("[$food]")
+    }
+}
+class Cat(override val name: String, private val food: String = "Рыба" ): Tamagotchi(""){
+    override fun printFoodInfo() {
+        super.printFoodInfo()
+        println("[$food]")
+    }
+}
+
+fun main(){
+    val fox = Fox("Лиса")
+    val dog = Dog("Собака")
+    val cat = Cat("Кошка")
+
+    val list: List<Tamagotchi> = listOf(fox, dog, cat)
+
+    for (element in list){
+        element.printFoodInfo()
+    }
+}

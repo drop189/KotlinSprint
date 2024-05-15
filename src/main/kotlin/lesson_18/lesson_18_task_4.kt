23cbe0858b3339af5b59351b504a8cdef2fe86b1
drop189 <71636973+drop189@users.noreply.github.com>
2024-05-16 00:11:22 +0300

    KS-18-4 do task4

--- src/main/kotlin/lesson_18/lesson_18_task_4.kt
+++ src/main/kotlin/lesson_18/lesson_18_task_4.kt
@@ -1,30 +1,30 @@
 package org.example.lesson_18
 
-open class Parcel(){
-    open fun calculateAreaOfSurface(){
+open class Parcel() {
+    open fun calculateAreaOfSurface() {
         print("Площадь поверхности посылки: ")
     }
 }
 
-class RectangleParcel(private val length: Int, private val width: Int, private val height: Int): Parcel(){
+class RectangleParcel(private val length: Int, private val width: Int, private val height: Int) : Parcel() {
     override fun calculateAreaOfSurface() {
         super.calculateAreaOfSurface()
-val result = (2 * ((length * width) + (width * height) + (height * length)) )
+        val result = (2 * ((length * width) + (width * height) + (height * length)))
         println("$result")
     }
 }
 
-class CubeParcel(private val length: Int): Parcel(){
+class CubeParcel(private val length: Int) : Parcel() {
     override fun calculateAreaOfSurface() {
         super.calculateAreaOfSurface()
-val result = (6 * length * length)
+        val result = (6 * length * length)
         println("$result")
     }
 }
 
 fun main() {
     val cubeParcel = CubeParcel(4)
-    val rectangleParcel = RectangleParcel(3,4,5)
+    val rectangleParcel = RectangleParcel(3, 4, 5)
 
     cubeParcel.calculateAreaOfSurface()
     rectangleParcel.calculateAreaOfSurface()

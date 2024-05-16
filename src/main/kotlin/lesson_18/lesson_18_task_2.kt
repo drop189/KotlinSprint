1ef22a10634bbd1134a72724488ac0f259104264
drop189 <71636973+drop189@users.noreply.github.com>
2024-05-16 12:35:25 +0300

    KS-18-2 refresh task2

--- src/main/kotlin/lesson_18/lesson_18_task_2.kt
+++ src/main/kotlin/lesson_18/lesson_18_task_2.kt
@@ -1,38 +1,22 @@
 package org.example.lesson_18
 
-open class Dice {
+abstract class Dice(private val facets: Int) {
     open fun rollDices() {
         print("Бросаем кости")
         repeat(3) {
             print(".")
             Thread.sleep(1000)
         }
-    }
-}
-
-class Dice4(private val facets: Int = 4) : Dice() {
-    override fun rollDices() {
-        super.rollDices()
         val result = (1..facets).random()
         println("Результат $result")
     }
 }
 
-class Dice6(private val facets: Int = 6) : Dice() {
-    override fun rollDices() {
-        super.rollDices()
-        val result = (1..facets).random()
-        println("Результат $result")
-    }
-}
+class Dice4: Dice(4)
 
-class Dice8(private val facets: Int = 8) : Dice() {
-    override fun rollDices() {
-        super.rollDices()
-        val result = (1..facets).random()
-        println("Результат $result")
-    }
-}
+class Dice6: Dice(6)
+
+class Dice8: Dice(8)
 
 fun main() {
     val dice4 = Dice4()

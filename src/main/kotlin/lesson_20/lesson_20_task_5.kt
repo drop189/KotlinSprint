89790dd7a540009ad01ab6421b308c418fa87a14
drop189 <71636973+drop189@users.noreply.github.com>
2024-05-16 23:39:11 +0300

    KS-20-5 refresh task5

--- src/main/kotlin/lesson_20/lesson_20_task_5.kt
+++ src/main/kotlin/lesson_20/lesson_20_task_5.kt
@@ -1,23 +1,26 @@
 package org.example.lesson_20
 
-val randomPhrase = listOf(
-    "Приветствую человек", "Ты слишком медленный", "Хороших выходных",
-    "Можешь не волноваться на счет ошибок в коде, потом сам исправлю",
-    "Воды нет, растительности нет"
-).random()
+class Robot {
+    val randomPhrase = listOf(
+        "Приветствую человек", "Ты слишком медленный", "Хороших выходных",
+        "Можешь не волноваться на счет ошибок в коде, потом сам исправлю",
+        "Воды нет, растительности нет"
+    ).random()
 
-val invertingModifier: (String) -> String = { it: String -> it.reversed() }
+    val invertingModifier: (String) -> String = { it: String -> it.reversed() }
 
-fun say(phrase: String) {
-    println(phrase)
-}
+    fun say(phrase: String) {
+        println(phrase)
+    }
 
-fun setModifier(modifier: (String) -> String): String {
-    return modifier(randomPhrase)
+    fun setModifier(modifier: (String) -> String): String {
+        return modifier(randomPhrase)
+    }
 }
 
 fun main() {
-    say(randomPhrase)
-    val phrase = setModifier(invertingModifier)
-    say(phrase)
+    val robot = Robot()
+    robot.say(robot.randomPhrase)
+    val phrase = robot.setModifier(robot.invertingModifier)
+    robot.say(phrase)
 }

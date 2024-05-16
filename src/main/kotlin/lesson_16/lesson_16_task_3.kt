a69c2a358b516c48369cdbc693eebfd09545e5c4
drop189 <71636973+drop189@users.noreply.github.com>
2024-05-16 11:41:33 +0300

    KS-16-3 refresh task3

--- src/main/kotlin/lesson_16/lesson_16_task_3.kt
+++ src/main/kotlin/lesson_16/lesson_16_task_3.kt
@@ -1,15 +1,15 @@
 package org.example.lesson_16
 
-class User(){
-    val login: String = "login"
-    private val password = "password"
-
+class User(
+    private val login: String,
+    private val password: String,
+) {
     fun checkPassword(passwordInput: String) = passwordInput == password
 }
 
 fun main() {
-    val user = User()
-    user.login
+    val user = User("login", "password")
+
     val passwordInput1 = user.checkPassword("")
     val passwordInput2 = user.checkPassword("4")
     val passwordInput3 = user.checkPassword("password")

cafa59a23e36fc089a63d104b2f190a93b1d53d0
drop189 <71636973+drop189@users.noreply.github.com>
2024-05-16 20:22:18 +0300

    KS-19-3 do task3

---
+++ src/main/kotlin/lesson_19/lesson_19_task_3.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_19
+
+class SpaceShip(){
+    fun fly(){
+        println("Летит")
+        //TODO: Нужна информация о дополнительной логике
+    }
+
+    fun shoot(){
+        TODO("Прописать логику стрельбы")
+    }
+}
+
+fun main(){
+    val spaceShip = SpaceShip()
+    spaceShip.fly()
+    spaceShip.shoot()
+}

b8c0fdc21491ca4431805d25c43c6182008ea952
drop189 <71636973+drop189@users.noreply.github.com>
2024-05-17 11:31:54 +0300

    KS-18-5 refresh task5

--- src/main/kotlin/lesson_18/lesson_18_task_5.kt
+++ src/main/kotlin/lesson_18/lesson_18_task_5.kt
@@ -6,18 +6,43 @@ class Lap(val center: Dot, val edge: Dot)
 
 class Screen {
     fun draw(dot: Dot) {
-        println("Dot(x=${dot.x}, y=${dot.y})")
+        println("Dot[x=${dot.x}, y=${dot.y}]")
     }
 
     fun draw(square: Square) {
-        "Square" +
-                "(topLeft=${square.topLeft}," +
-                " topRight=${square.topRight}," +
-                " bottomLeft=${square.bottomLeft}," +
-                " bottomRight=${square.bottomRight})"
+        println(
+            "Square" +
+                    "(topLeft=[x=${square.topLeft.x},y=${square.topLeft.y}]," +
+                    " topRight=[x=${square.topRight.x},y=${square.topRight.y}]," +
+                    " bottomLeft=[x=${square.bottomLeft.x},y=${square.bottomLeft.y}]," +
+                    " bottomRight=[x=${square.bottomRight.x},y=${square.bottomRight.y}])"
+        )
     }
 
     fun draw(circle: Lap) {
-        println("Circle(center=${circle.center}, edge=${circle.edge})")
+        println(
+            "Circle(center=[x=${circle.center.x},y=${circle.center.y}], edge=[x=${circle.edge.x},y=${circle.edge.y}])"
+        )
     }
+}
+
+fun main() {
+    val screen = Screen()
+
+    val dot = Dot(10, 20)
+    screen.draw(dot)
+
+    val square = Square(
+        Dot(0, 10),
+        Dot(10, 10),
+        Dot(0, 0),
+        Dot(10, 0)
+    )
+    screen.draw(square)
+
+    val circle = Lap(
+        Dot(5, 5),
+        Dot(10, 5)
+    )
+    screen.draw(circle)
 }

fe0b1e5621bd382007580c12c8d60ab7587128d2
drop189 <71636973+drop189@users.noreply.github.com>
2024-05-17 16:03:35 +0300

    KS-19-4 refresh task4

--- src/main/kotlin/lesson_19/lesson_19_task_4.kt
+++ src/main/kotlin/lesson_19/lesson_19_task_4.kt
@@ -1,27 +1,18 @@
 package org.example.lesson_19
 
-enum class Ammo(val damage: Int?) {
-    NO_AMMO(null),
+enum class Ammo(val damage: Int) {
     BLUE(5),
     GREEN(10),
     RED(20),
 }
 
-class Tank(private var ammo: Ammo = Ammo.NO_AMMO) {
+class Tank(private var ammo: Ammo? = null) {
     fun shoot() {
-        println("Танк произвел выстрел на ${ammo.damage} урона")
+        println("Танк произвел выстрел на ${ammo?.damage} урона")
     }
 
-    fun changingAmmoBlue() {
-        ammo = Ammo.BLUE
-    }
-
-    fun changingAmmoGreen() {
-        ammo = Ammo.GREEN
-    }
-
-    fun changingAmmoRed() {
-        ammo = Ammo.RED
+    fun changingAmmo(newAmmo: Ammo) {
+        ammo = newAmmo
     }
 }
 
@@ -29,10 +20,10 @@ fun main() {
     val tank = Tank()
 
     tank.shoot()
-    tank.changingAmmoBlue()
+    tank.changingAmmo(Ammo.BLUE)
     tank.shoot()
-    tank.changingAmmoGreen()
+    tank.changingAmmo(Ammo.GREEN)
     tank.shoot()
-    tank.changingAmmoRed()
+    tank.changingAmmo(Ammo.RED)
     tank.shoot()
 }

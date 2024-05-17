49a2b03439cea5bc6e6faab37cb701385a2a0c53
drop189 <71636973+drop189@users.noreply.github.com>
2024-05-17 17:21:15 +0300

    KS-21-3 do task3

--- src/main/kotlin/lesson_21/lesson_21_task_3.kt
+++ src/main/kotlin/lesson_21/lesson_21_task_3.kt
@@ -1,2 +1,19 @@
 package org.example.lesson_21
 
+class ExtensionPlayer(
+    name: String,
+    var healthPoints: Int,
+    val maxHP: Int,
+)
+
+fun ExtensionPlayer.isHealthy(): Boolean {
+    return healthPoints == maxHP
+}
+
+fun main() {
+    val player1 = ExtensionPlayer("Healthy Player", 80, 80)
+    val player2 = ExtensionPlayer("Not Healthy Player", 52, 80)
+
+    println(player1.isHealthy())
+    println(player2.isHealthy())
+}

0e966e4bc8b82089c0cbf096ceb2716348d7477d
drop189 <71636973+drop189@users.noreply.github.com>
2024-05-17 15:57:40 +0300

    KS-19-3 refresh task3

--- src/main/kotlin/lesson_19/lesson_19_task_3.kt
+++ src/main/kotlin/lesson_19/lesson_19_task_3.kt
@@ -1,6 +1,6 @@
 package org.example.lesson_19
 
-class SpaceShip(){
+class SpaceShip{
     fun fly(){
         println("Летит")
         //TODO: Нужна информация о дополнительной логике
@@ -9,10 +9,16 @@ class SpaceShip(){
     fun shoot(){
         TODO("Прописать логику стрельбы")
     }
+
+    fun land(){
+        println("Приземляется")
+        //TODO: Нужна информация о дополнительной логике
+    }
 }
 
 fun main(){
     val spaceShip = SpaceShip()
     spaceShip.fly()
     spaceShip.shoot()
+    spaceShip.land()
 }

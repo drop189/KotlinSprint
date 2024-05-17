ba5f40581dc03e8c96136eefb8928a3c6a6213b4
drop189 <71636973+drop189@users.noreply.github.com>
2024-05-17 16:16:47 +0300

    KS-19-5 refresh task5

--- src/main/kotlin/lesson_19/lesson_19_task_5.kt
+++ src/main/kotlin/lesson_19/lesson_19_task_5.kt
@@ -11,23 +11,38 @@ class Human(
 )
 
 fun main() {
-    println("""Пожалуйста, введите данные о человеке в формате:
+    println(
+        """Пожалуйста, введите данные о человеке в формате:
         | Имя 
         | Пол 
         |В графу Пол допускается только ввод формата - (Мужской/Женский)
-        | """.trimMargin())
+        | """.trimMargin()
+    )
 
     val peopleList = mutableListOf<Human>()
 
 
     repeat(5) {
-        println("Введите имя и пол человека ${it + 1}: ")
+        println("Введите имя и пол человека ${it + 1} ")
+        print("Введите имя: ")
         val name = readln()
-        val userInput = readln()
-        val gender: Gender = when (userInput) {
-            "Мужской" -> Gender.MALE
-            "Женский" -> Gender.FEMALE
-            else -> throw IllegalArgumentException("Неправильный ввод пола. Используйте 'Мужской' или 'Женский'.")
+        print("Введите пол: ")
+        var userInput = readln()
+        var gender: Gender? = null
+
+        while (gender == null) {
+
+            when (userInput) {
+                "Мужской" -> gender = Gender.MALE
+                "Женский" -> gender = Gender.FEMALE
+                else -> {
+                    gender = null
+                    println("Неверный ввод")
+                    print("Введите пол:")
+                    userInput = readln()
+                }
+            }
+
         }
         val person = Human(name, gender)
 

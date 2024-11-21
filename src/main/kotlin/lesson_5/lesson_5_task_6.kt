b6f6804329e14df45444ad5f1552a1cc772d16ac
drop189 <71636973+drop189@users.noreply.github.com>
2024-11-21 22:48:09 +0300

    KS-5-6 do task6

---
+++ src/main/kotlin/lesson_5/lesson_5_task_6.kt
@@ -0,0 +1,42 @@
+package org.example.lesson_5
+
+import kotlin.math.pow
+
+private const val CENTIMETERS_IN_METERS = 100
+
+fun main() {
+    println("Калькулятор ИМТ")
+    println("Чтобы произвести расчёты, нам необходимо знать ваш вес в килограммах и ваш рост в сантиметрах.")
+    print("Введите ваш вес(кг): ")
+    val userWeight = readln().toFloatOrNull() ?: -1F
+    print("Введите ваш рост(см): ")
+    val userHeight = readln().toFloatOrNull() ?: -1F
+
+    val userHeightMeters = userHeight / CENTIMETERS_IN_METERS
+    val indexIMT = userWeight / userHeightMeters.pow(2)
+
+    var category = ""
+    if (indexIMT.isNaN() || indexIMT == Float.POSITIVE_INFINITY || indexIMT < 0){
+        println("Были введены не реалистичные данные, программа вынуждена завершить работу.")
+        return
+    }
+    if (indexIMT < 18.5) {
+        category = "Недостаточная масса тела"
+        println("- ИМТ < 18.5: $category")
+    }
+    if (indexIMT >= 18.5 && indexIMT < 25) {
+        category = "Нормальная масса тела"
+        println("- 18.5 ≤ ИМТ < 25: $category")
+    }
+    if (indexIMT >= 25 && indexIMT < 30) {
+        category = "Избыточная масса тела"
+        println("- 25 ≤ ИМТ < 30: $category")
+    }
+    if (indexIMT >= 30) {
+        category = "Ожирение"
+        println("- ИМТ ≥ 30: $category")
+    }
+
+    println("Результаты:")
+    println("ИМТ - ${indexIMT}, $category")
+}

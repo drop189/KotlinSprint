675cedb0e455371c35a55cfc327ca33c2dcb6637
drop189 <71636973+drop189@users.noreply.github.com>
2024-11-23 12:29:16 +0300

    KS-5-6 update task6

--- src/main/kotlin/lesson_5/lesson_5_task_6.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_6.kt
@@ -15,26 +15,37 @@ fun main() {
     val userHeightMeters = userHeight / CENTIMETERS_IN_METERS
     val indexIMT = userWeight / userHeightMeters.pow(2)
 
-    var category = ""
-    if (indexIMT.isNaN() || indexIMT == Float.POSITIVE_INFINITY || indexIMT < 0){
-        println("Были введены не реалистичные данные, программа вынуждена завершить работу.")
-        return
-    }
-    if (indexIMT < 18.5) {
-        category = "Недостаточная масса тела"
-        println("- ИМТ < 18.5: $category")
-    }
-    if (indexIMT >= 18.5 && indexIMT < 25) {
-        category = "Нормальная масса тела"
-        println("- 18.5 ≤ ИМТ < 25: $category")
-    }
-    if (indexIMT >= 25 && indexIMT < 30) {
-        category = "Избыточная масса тела"
-        println("- 25 ≤ ИМТ < 30: $category")
-    }
-    if (indexIMT >= 30) {
-        category = "Ожирение"
-        println("- ИМТ ≥ 30: $category")
+    val category: String
+    when {
+        indexIMT.isNaN() || indexIMT == Float.POSITIVE_INFINITY || indexIMT < 0 -> {
+            println("Были введены не реалистичные данные, программа вынуждена завершить работу.")
+            return
+        }
+
+        indexIMT < 18.5 -> {
+            category = "Недостаточная масса тела"
+            println("- ИМТ < 18.5: $category")
+        }
+
+        indexIMT >= 18.5 && indexIMT < 25 -> {
+            category = "Нормальная масса тела"
+            println("- 18.5 ≤ ИМТ < 25: $category")
+        }
+
+        indexIMT >= 25 && indexIMT < 30 -> {
+            category = "Избыточная масса тела"
+            println("- 25 ≤ ИМТ < 30: $category")
+        }
+
+        indexIMT >= 30 -> {
+            category = "Ожирение"
+            println("- ИМТ ≥ 30: $category")
+        }
+
+        else -> {
+            println("Ошибка, завершение работы.")
+            return
+        }
     }
 
     println("Результаты:")

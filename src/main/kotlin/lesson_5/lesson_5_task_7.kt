2aba646205d3d437799a33d610548126a6a5977c
drop189 <71636973+drop189@users.noreply.github.com>
2024-11-23 12:37:01 +0300

    KS-5-7 update task7

--- src/main/kotlin/lesson_5/lesson_5_task_7.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_7.kt
@@ -1,8 +1,9 @@
 package org.example.lesson_5
 
 fun main() {
-    println("`Калькулятор топлива`")
-    println("Для расчета стоимости топлива введите следующие данные")
+    println("`Калькулятор топлива`\n" +
+            "Для расчета стоимости топлива введите следующие данные\n")
+
     print("Расстояние поездки (в километрах): ")
     val distance = readln().toFloat()
     print("Расход топлива на 100 км (в литрах): ")
@@ -13,6 +14,7 @@ fun main() {
     val fuel = (distance * fuelConsumption) / 100
     val totalCost = fuel * priceForFuel
 
+    println("")
     println(
         """Результаты:
         |Общее количество топлива - ${String.format("%.2f", fuel)} литров
